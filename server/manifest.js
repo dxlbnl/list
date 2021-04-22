88f4e8cf1123ed033ba9c1731cd005245880b43f
Dexter <a.esselink@gmail.com>
2021-04-22 15:30:17 +0200

    logging manifest

--- server/manifest.js
+++ server/manifest.js
@@ -24,11 +24,13 @@ export function manifest(request, response) {
   if (!referer) {
     let [, id] = new URL(referer).pathname.match(/^\/([a-zA-Z0-9.+_-]*)/)
 
+    console.log("Creating manifest:", id)
     response.writeHead(200, '', {
       'Content-Type': 'application/json'
     })
     response.write(JSON.stringify({
       ...baseManifest,
+      short_name: id,
       start_url: `/${id}`
     }))
     response.send()
