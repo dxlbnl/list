860543da4a379a71070079e4d106b63033178fac
Dexter <dexter@dxlb.nl>
2026-04-18 19:50:52 +0200

    removed old codebase

--- README.md
+++
@@ -1,5 +0,0 @@
-# Synchronizing lists app
-
-Create a list and a random url
-Share that url with your peers
-Synchronizes all edits.
--- src/lib/SlotTester.svelte
+++
@@ -1,9 +0,0 @@
-<script lang='ts'>
-	export let Component: ConstructorOfATypedSvelteComponent;
-
-	export let props = {}
-</script>
-
-<svelte:component this={Component} {...props}>
-	<h1 data-testid="slot">Test Data</h1>
-</svelte:component>
